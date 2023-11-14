4a4686ef2fe067c7284011368bf1e070e127bedc
hafiz <hafiz.jaharudin@kloudius.com>
2023-11-14 16:32:03 +0800

    add function close toolbar in BaseActivity

--- app/src/main/java/com/example/tictactoe/base/BaseActivity.kt
+++ app/src/main/java/com/example/tictactoe/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoe.base
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.example.tictactoe.R
@@ -23,5 +24,14 @@ abstract class BaseActivity<Binding : ViewBinding> : AppCompatActivity() {
     private fun setupToolBar() {
         setSupportActionBar(binding.root.findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> { onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
